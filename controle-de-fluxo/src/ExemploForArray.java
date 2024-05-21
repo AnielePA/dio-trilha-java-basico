5083c8af50f5521a06e6e668111775ca3fd89550
aniele <ardissonaniele99@gmail.com>
2024-05-21 10:29:54 -0400

    exemplos sobre controle de fluxo

---
+++ controle-de-fluxo/src/ExemploForArray.java
@@ -0,0 +1,13 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        
+        //em arrays o índice de elementos inicia em ZERO
+        String frutas [] =  {"BANANA","FRUTA PÃO","MORANGO","LARANJA", "CUPUAÇU", "LIMÃO"};
+	
+             //Forma abreviada
+	    for(String fruta : frutas) {
+            System.out.println("O nome da fruta é: "+ fruta);
+	}
+
+    }
+}
